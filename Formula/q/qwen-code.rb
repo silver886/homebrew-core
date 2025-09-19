45cae61cf847c250d58c3f9aab29ce2c84c8e06b
Patrick Linnane <patrick@linnane.io>
2025-09-19 08:19:49 -0700

    qwen-code: bump to 0.0.12 & add livecheck
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/q/qwen-code.rb
+++ Formula/q/qwen-code.rb
@@ -1,9 +1,14 @@
 class QwenCode < Formula
   desc "AI-powered command-line workflow tool for developers"
   homepage "https://github.com/QwenLM/qwen-code"
-  url "https://registry.npmjs.org/@qwen-code/qwen-code/-/qwen-code-0.0.11.tgz"
-  sha256 "88a561e5882793c52ec6e6946eb736b418501cacbeceb47120ce25f87db22894"
+  url "https://registry.npmjs.org/@qwen-code/qwen-code/-/qwen-code-0.0.12.tgz"
+  sha256 "439fdd35bc2f7e0cc2cc7a20f927a8184b98319e57049128f3fe4b8629c6b792"
   license "Apache-2.0"
+
+  livecheck do
+    url :homepage
+    regex(/(\d+(?:\.\d+)+)/i)
+  end
 
   bottle do
     sha256                               arm64_tahoe:   "4108026e9bb486f365b7c49eabe675244eb57885edb04fffbd2f477b59760f4c"
